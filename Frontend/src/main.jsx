a6b43918cc584daa99daac1ca0bb1b21f11d1a9c
Piyush367762 <piyush9c24@gmail.com>
2025-09-12 21:30:55 +0530

    first commit

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LoginPage from './LoginPage.jsx';
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<LoginPage />);
