f1dff93ea73c26ea226a7ae6ea79df6b43f8bb77
Piyush367762 <piyush9c24@gmail.com>
2025-09-14 08:51:39 +0530

    minor update

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -8,10 +8,10 @@ export default function NavBar() {
       <div className="logo">Pic</div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/how-it-works">How it works</Link></li>
-        <li><Link to="/features">Features</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/Study-Material">Study Material</Link></li>
         <li><Link to="/scholarships">Scholarships</Link></li>
-        <li><Link to="/faq">FAQ</Link></li>
+        <li><Link to="/college">College</Link></li>
         <li><Link to="/">Language</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
         <li><Link to="/login">Log In</Link></li>
